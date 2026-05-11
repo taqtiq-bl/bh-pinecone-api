3c770cbe321970609644e4453301f2b72a8ea8c2
taqtiq-bl <bl@taqtiq.net>
2026-05-11 22:22:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -89,10 +89,10 @@ def search():
 def health():
     return jsonify({'status': 'ok'})
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
 @app.route('/logo.jpg')
 def logo():
     return send_from_directory('.', 'Logo_b+h_Claim_flaeche_farbe.jpg')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
